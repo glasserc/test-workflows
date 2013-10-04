db5e28432047c493896135773f481061fa8f73f7
Ethan Glasser-Camp <ethan@betacantrips.com>
2013-10-04 17:32:48 -0400

    Try to add another program

---
+++ aux.py
@@ -0,0 +1,10 @@
+"""
+This is an auxiliary program that might be useful someday.
+"""
+
+def aux():
+    """Auxiliary program."""
+    return "Hello, world!"
+
+if __name__ == '__main__':
+    assert aux()
